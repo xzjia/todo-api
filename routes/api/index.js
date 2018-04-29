9b40ae5578cec4d56ba21caad78d6b6339396e0c
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2018-04-29 15:59:51 +0900

    Change the endpoint to contain API version

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const userRouter = require("./user");
 
 module.exports = services => {
-  router.use("/users", userRouter(services));
+  router.use("/users/v1.0", userRouter(services));
 
   return router;
 };
